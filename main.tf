638fce2199d115456f3945e80b86dc8826ab3caf
Sai Srikanth <saisrikanth@Sais-MacBook-Pro.local>
2019-08-10 13:54:38 -0400

    terrafor-ec2-backend

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws"{
+
+	region = "us-east-1"
+}
+
+resource "aws_instance" "example"{
+	ami = "ami-078ba39c4a35d4be8"
+	instance_type = "t2.micro"
+	security_groups = ["tf-test1"]
+	key_name = "tf-ubuntu-1"
+	tags = {
+    Name = "Tomcat-prac"
+  }
+	
+}
