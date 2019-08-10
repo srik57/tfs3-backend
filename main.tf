9b4f37ffeb9647898fe59f30501ce8c96c4aa6f0
Sai Srikanth <saisrikanth@Sais-MacBook-Pro.local>
2019-08-10 14:55:34 -0400

    first

--- main.tf
+++ main.tf
@@ -2,6 +2,13 @@ provider "aws"{
 
 	region = "us-east-1"
 }
+terraform {
+  backend "s3" {
+    bucket = "srikanth-s3-tfbucket"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 
 resource "aws_instance" "example"{
 	ami = "ami-078ba39c4a35d4be8"
